488be474bdc73ea252770e830051c1a6e059f213
semino <semino@posteo.com>
2026-06-16 13:44:16 +0200

    day2 2015: python

---
+++ 2015/python/day2.py
@@ -0,0 +1,31 @@
+def read_data():
+    with open("../data/day2.dat") as f:
+        return [
+            tuple(map(int, line.strip().split("x"))) for line in f
+        ]
+
+
+def part1(puzzle_input):
+    total = 0
+    for l, w, h in puzzle_input:
+        areas = [h * w, w * l, l * h]
+        total += 2 * sum(areas) + min(areas)    
+
+
+    return total
+
+def part2(puzzle_input):
+    total = 0
+    for l, w, h in puzzle_input:
+        wrap  =  2 * (h + l + w - max(h,l,w))
+        total += wrap + h * w * l    
+
+
+    return total
+
+
+
+if __name__ == "__main__":
+    data = read_data()
+    print(f"part1: {part1(data)}")
+    print(f"part2: {part2(data)}")
